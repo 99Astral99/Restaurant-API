bedcb707c1a7696f87f9bb77779fc0674c0587ca
Viktor <strik4462@gmail.com>
2023-11-13 12:57:39 +0300

    Added Docker support to WebAPI and PostgreSQL DB

--- Restaraunt.Persistence/DependencyInjection.cs
+++ Restaraunt.Persistence/DependencyInjection.cs
@@ -10,7 +10,13 @@ namespace Restaraunt.Persistence
 		public static IServiceCollection AddPersistence(this IServiceCollection services,
 			IConfiguration configuration)
 		{
-			var connectionString = configuration["ConnectionStrings:pg-connection"];
+
+			var dbHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+			var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+			var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+			var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+			var connectionString = $"Host={dbHost};User Id={dbUser};Password={dbPassword};Port=5432;Database={dbName}";
+
 			services.AddDbContext<ProductDbContext>(options =>
 			{
 				options.UseNpgsql(connectionString);
