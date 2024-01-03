59fa040f3a51caa79f230a3d2ca938389c734b83
Viktor <strik4462@gmail.com>
2024-01-03 12:06:38 +0300

    clean up some sections of code

--- Restaraunt.Application/BookingTableOrders/Commands/CreateBookingTableOrder/CreateBookingTableOrderCommand.cs
+++ Restaraunt.Application/BookingTableOrders/Commands/CreateBookingTableOrder/CreateBookingTableOrderCommand.cs
@@ -12,11 +12,3 @@ namespace Restaraunt.Application.BookingTableOrders.Commands.CreateBookingTableO
 }
 
 
-
-//public Guid Id { get; set; }
-//public int ReservationTableId { get; set; }
-//public virtual ReservationTable ReservationTable { get; set; }
-//public int TableNumber { get; set; }
-//public string ClientName { get; set; }
-//public int ReservedPeopleAmount { get; set; }
-//public DateTime Date { get; set; }
