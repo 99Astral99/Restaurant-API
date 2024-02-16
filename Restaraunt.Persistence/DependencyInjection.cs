3b7d99cdd125bff10ca64304158bb0b7f173102a
Viktor <strik4462@gmail.com>
2024-02-16 18:27:39 +0300

    deleted unuseful comment

--- Restaraunt.Persistence/DependencyInjection.cs
+++ Restaraunt.Persistence/DependencyInjection.cs
@@ -10,14 +10,6 @@ namespace Restaraunt.Persistence
 		public static IServiceCollection AddPersistence(this IServiceCollection services,
 			IConfiguration configuration)
 		{
-
-			//var dbHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
-			//var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
-			//var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-			//var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
-			//var connectionString = $"Host={dbHost};User Id={dbUser};Password={dbPassword};Port=5432;Database={dbName}";
-			
-			//if you'll want to use a localhost
 			var connectionString = configuration["ConnectionStrings:pg-connection"];
 
 			services.AddDbContext<ProductDbContext>(options =>
